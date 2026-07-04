d7fe5a5e20d988701cac93cb8320f4631ef7bc11
Dayana6d <dayana.rivera2028@hilasal.superate.org>
2026-07-03 19:34:50 -0600

    Trabajo de Dayana

--- Datos Dayana.py
+++ Datos Dayana.py
@@ -5,10 +5,7 @@ idioma = "Español"
 estatura = "1.55 m"    
 comida_favorita = "Pescado"    
 color_favorito = "Azul"
-carrera = "Negocios Internacionales"
-pasatiempo = "Escribir poesía y libros"
-meta = "Crear una carrera internacional en negocios y tecnología"
- 
+
  
 print(f"Nombre completo : {nombre}")
 print(f"Edad            : {edad}")
@@ -17,9 +14,3 @@ print(f"Idioma natal    : {idioma}")
 print(f"Estatura        : {estatura}")
 print(f"Color favorito  : {color_favorito}")
 print(f"Comida favorita : {comida_favorita}")
-print(f"Carrera soñada  : {carrera}")
-print(f"Pasatiempo      : {pasatiempo}")
-print(f"Meta principal  : {meta}")
- 
-print("thank you for see my code")
-print("wooooooow")
